3666c278a5c4795f99edcc87a0bf1d5deb6389e8
saurabhdemo <saurabhmehta82@gmail.com>
2023-04-19 13:14:15 +0530

    server code change

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 const http = require('http');
-const http = require('https');
+const https = require('https');
+const bodyParser = require('body-parser');
 
-// Create a server to receive requests
+// Create a server to receive POST requests from SNS
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        if (data.Type === 'SubscriptionConfirmation') {
+          const subscribeUrl = data.SubscribeURL;
+          console.log('Received subscription confirmation message:', data);
+          console.log('Visit this URL to confirm subscription:', subscribeUrl);
+        } else {
+          console.log('Received message:', data);
+        }
+        res.end('OK');
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+        res.statusCode = 400;
+        res.end('Bad Request');
+      }
+    });
+  } else {
+    res.statusCode = 405;
+    res.end('Method Not Allowed');
+  }
 });
 
-// Start the server on the specified port
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start the server on port 3000
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
+
